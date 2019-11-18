79d1e03c7fbf66321d981b1cbb9967bb45c9728b
johnsnow237 <johnsnow237@localhost.localdomain>
2019-11-18 18:06:43 +0100

    Creation de la branche test_new_branch + modif test.py

--- test.py
+++ test.py
@@ -13,10 +13,6 @@ founding_members = ()
 
 songs = {}
 
-print ("La variable group est du type : " + str(type (group)))
-print ("La variable year est du type : " + str(type (year)))
-print ("La variable still_together est du type : " + str(type (still_together)))
-print ("La variable albums est du type : " + str(type (albums)))
-print ("La variable founding_members est du type : " + str(type (founding_members)))
-print ("La variable songs est du type : " + str(type (songs)))
+print("ici on va developper une idée secondaire")
+
 
